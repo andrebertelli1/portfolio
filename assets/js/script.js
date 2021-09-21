1ba150eedbf6a2bb26370d85ee3ce0c30840042d
Andre Bertelli <andre_bertelli@outlook.com>
2021-09-21 00:00:31 -0300

    responsividade

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,3 +44,15 @@ if (target.length) {
     animeScroll();
   }, 200));
 }
+
+let show = true;
+
+const menuSection = document.querySelector(".menu-section")
+const menuToggle = menuSection.querySelector(".menu-toggle")
+
+menuToggle.addEventListener("click", () => {
+  document.body.style.overflow = show ? "hidden" : "initial"
+
+  menuSection.classList.toggle("on", show)
+  show = !show;
+})
